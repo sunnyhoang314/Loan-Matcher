ed43026646a9d0a1e63cefeb86408ca527654331
Luftwaffles <sunnyhoang314@gmail.com>
2024-11-16 18:33:24 -0700

    Added constraints

--- database.sql
+++ database.sql
@@ -10,13 +10,13 @@ CREATE TABLE CLIENT
         CEmail             VARCHAR(20)         NOT NULL,
         Fname              VARCHAR(15)         NOT NULL,
         Lname              VARCHAR(15)         NOT NULL,
-        Credit             INT,
+        Credit             INT                 NOT NULL,
         CPassword          VARCHAR             NOT NULL,
-        CLocation          VARCHAR,
-        DOB/Establishment  DATE,
-        CPhone             CHAR(10),
-        Financial_details  VARCHAR,
-        PRIMARY KEY(CEmail),
+        CLocation          VARCHAR             NOT NULL,
+        DOB/Establishment  DATE                NOT NULL,
+        CPhone             CHAR(10)            NOT NULL,            
+        Financial_details  VARCHAR             NOT NULL,
+        CONSTRAINT CPK PRIMARY KEY(CEmail),
         CONSTRAINT CPW CHECK(LENGTH(CPassword) >= 8 AND CPassword ~ '^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=\-{}\[\]:;"''<>,.?/]).+$')
     );
 
@@ -26,57 +26,64 @@ CREATE TABLE LOAN_PROVIDER
         LEmail             VARCHAR(20)         NOT NULL,
         Fname              VARCHAR(15)         NOT NULL,
         Lname              VARCHAR(15)         NOT NULL,
-        Terms              VARCHAR,
+        Terms              VARCHAR             NOT NULL,
         LPassword          VARCHAR             NOT NULL,
-        LLocation          VARCHAR,
-        LPhone             CHAR(10),
-        LicenseNo          CHAR(10),
-        PRIMARY KEY(LEmail),
+        LLocation          VARCHAR             NOT NULL,
+        LPhone             CHAR(10)            NOT NULL,
+        LicenseNo          CHAR(10)            NOT NULL,
+        CONSTRAINT LPK PRIMARY KEY(LEmail),
         CONSTRAINT LPW CHECK(LENGTH(LPassword) >= 8 AND LPassword ~ '^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=\-{}\[\]:;"''<>,.?/]).+$')
     );
 
 -- Create the LOAN_POST table
 CREATE TABLE LOAN_POST
     (
-        PName              VARCHAR,
-        MaxRate            INT,
-        MinTermL           DATE,
-        MaxTermL           DATE,
-        PStatus            VARCHAR,
-        LType              VARCHAR,
+        PName              VARCHAR             NOT NULL,
+        MaxRate            INT                 NOT NULL,
+        MinTermL           DATE                NOT NULL,
+        MaxTermL           DATE                NOT NULL,
+        PStatus            VARCHAR             NOT NULL,
+        LType              VARCHAR             NOT NULL,
         P_ID               CHAR(10)            NOT NULL,
-        Desired_amt        VARCHAR,
+        Desired_amt        VARCHAR             NOT NULL,
         CEmail             VARCHAR(20),
-        M_ID               CHAR(10)            NOT NULL,           
-        PRIMARY KEY(P_ID),
-        FOREIGN KEY(CEmail) REFERENCES CLIENT(CEmail)
+        M_ID               CHAR(10),          
+        CONSTRAINT LPPK PRIMARY KEY(P_ID),
+        CONSTRAINT LPSK UNIQUE(CEmail),
+        CONSTRAINT LPSK2 UNIQUE(M_ID),
+        CONSTRAINT LPFK FOREIGN KEY(CEmail) REFERENCES CLIENT(CEmail),
+        CONSTRAINT LPFK2 FOREIGN KEY(M_ID) REFERENCES MATCHED_POST(M_ID)
     );
 
 -- Create the LOAN_OFFER table
 CREATE TABLE LOAN_OFFER
     (
-        OName              VARCHAR,
-        MinRate            INT,
-        MinTermL           DATE,
-        MaxTermL           DATE,
-        OStatus            VARCHAR,
-        LType              VARCHAR,
+        OName              VARCHAR             NOT NULL,
+        MinRate            INT                 NOT NULL,
+        MinTermL           DATE                NOT NULL,
+        MaxTermL           DATE                NOT NULL,
+        OStatus            VARCHAR             NOT NULL,
+        LType              VARCHAR             NOT NULL,
         O_ID               CHAR(10),           NOT NULL,
-        Min_amt            VARCHAR,
-        Max_amt            VARCHAR,
-        LEmail             VARCHAR(20)         NOT NULL,
-        PRIMARY KEY(O_ID),
-        FOREIGN KEY(LEmail) REFERENCES LOAN_PROVIDER(LEmail)
+        Min_amt            VARCHAR             NOT NULL,
+        Max_amt            VARCHAR             NOT NULL,
+        LEmail             VARCHAR(20),
+        M_ID               CHAR(10),
+        CONSTRAINT LOPK PRIMARY KEY(O_ID),
+        CONSTRAINT LOSK UNIQUE(LEmail),
+        CONSTRAINT LOSK2 UNIQUE(M_ID),
+        CONSTRAINT LOFK FOREIGN KEY(LEmail) REFERENCES LOAN_PROVIDER(LEmail),
+        CONSTRAINT LOFK2 FOREIGN KEY(M_ID) REFERENCES MATCHED_POST(M_ID)
     );
 
 -- Create the ADMIN table
 CREATE TABLE ADMIN
     (
         AEmail             VARCHAR(20)         NOT NULL,
-        Fname              VARCHAR,
-        Lname              VARCHAR,
-        APassword          VARCHAR,
-        PRIMARY KEY(AEmail)
+        Fname              VARCHAR             NOT NULL,
+        Lname              VARCHAR             NOT NULL,
+        APassword          VARCHAR             NOT NULL,
+        CONSTRAINT APK PRIMARY KEY(AEmail)
     );
 
 -- Create the MATCHED_POST table
@@ -85,72 +92,80 @@ CREATE TABLE MATCHED_POST
         MRate           INT,
         MDate           DATE,
         MStatus         VARCHAR,
-        M_ID            CHAR(10),
-        P_ID            CHAR(10)            NOT NULL,
-        O_ID            CHAR(10)            NOT NULL,
+        M_ID            CHAR(10)            NOT NULL,
+        P_ID            CHAR(10),            
+        O_ID            CHAR(10),          
         CDecision       BOOLEAN,
         LDecision       BOOLEAN,
-        CEmail          VARCHAR(20)         NOT NULL,
-        LEmail          VARCHAR(20)         NOT NULL,
-        PRIMARY KEY(M_ID),
-        FOREIGN KEY(P_ID) REFERENCES LOAN_POST(P_ID),
-        FOREIGN KEY(O_ID) REFERENCES LOAN_OFFER(O_ID),
-        FOREIGN KEY(CEmail) REFERENCES CLIENT(CEmail),
-        FOREIGN KEY(LEmail) REFERENCES LOAN_PROVIDER(LEmail)
+        CEmail          VARCHAR(20),         
+        LEmail          VARCHAR(20),         
+        CONSTRAINT MPPK PRIMARY KEY(M_ID),
+        CONSTRAINT MPSK UNIQUE(P_ID),
+        CONSTRAINT MPSK2 UNIQUE(O_ID),
+        CONSTRAINT MPSK3 UNIQUE(CEmail),
+        CONSTRAINT MPSK4 UNIQUE(LEmail),
+        CONSTRAINT MPFK FOREIGN KEY(P_ID) REFERENCES LOAN_POST(P_ID),
+        CONSTRAINT MPFK2 FOREIGN KEY(O_ID) REFERENCES LOAN_OFFER(O_ID),
+        CONSTRAINT MPFK3 FOREIGN KEY(CEmail) REFERENCES CLIENT(CEmail),
+        CONSTRAINT MPFK4 FOREIGN KEY(LEmail) REFERENCES LOAN_PROVIDER(LEmail)
     );
 
 -- Create the CLIENT_DASHBOARD table
 CREATE TABLE CLIENT_DASHBOARD
     (
         CD_ID       CHAR(10)            NOT NULL,
-        P_ID        CHAR(10)            NOT NULL,
-        CEmail      VARCHAR(20)         NOT NULL,
-        PRIMARY KEY(CD_ID),
-        FOREIGN KEY(P_ID) REFERENCES LOAN_POST(P_ID),
-        FOREIGN KEY(CEmail) REFERENCES CLIENT(CEmail)
+        P_ID        CHAR(10),          
+        CEmail      VARCHAR(20),        
+        CONSTRAINT CDPK PRIMARY KEY(CD_ID),
+        CONSTRAINT CDSK UNIQUE(P_ID),
+        CONSTRAINT CDSK2 UNIQUE(CEmail),
+        CONSTRAINT CPFK FOREIGN KEY(P_ID) REFERENCES LOAN_POST(P_ID),
+        CONSTRAINT CDFK2 FOREIGN KEY(CEmail) REFERENCES CLIENT(CEmail)
     );
 
 -- Create the PROVIDER_DASHBOARD table
 CREATE TABLE PROVIDER_DASHBOARD
     (
         PD_ID       CHAR(10)            NOT NULL,
-        O_ID        CHAR(10)            NOT NULL,
-        LEmail      VARCHAR(20)         NOT NULL,
-        PRIMARY KEY(PD_ID),
-        FOREIGN KEY(O_ID) REFERENCES LOAN_OFFER(O_ID),
-        FOREIGN KEY(LEmail) REFERENCES LOAN_PROVIDER(LEmail)
+        O_ID        CHAR(10)            
+        LEmail      VARCHAR(20)         
+        CONSTRAINT PDPK PRIMARY KEY(PD_ID),
+        CONSTRAINT PDSK UNIQUE(O_ID),
+        CONSTRAINT PDSK2 UNIQUE(LEmail),
+        CONSTRAINT PDFK FOREIGN KEY(O_ID) REFERENCES LOAN_OFFER(O_ID),
+        CONSTRAINT PDFK2 FOREIGN KEY(LEmail) REFERENCES LOAN_PROVIDER(LEmail)
     );
 
 -- Create the POST_MATCH table
 CREATE TABLE POST_MATCH
     (
-        P_ID        CHAR(10)            NOT NULL,
-        O_ID        CHAR(10)            NOT NULL,
-        M_ID        CHAR(10)            NOT NULL,
-        FOREIGN KEY(P_ID) REFERENCES LOAN_POST(P_ID),
-        FOREIGN KEY(O_ID) REFERENCES LOAN_OFFER(O_ID),
-        FOREIGN KEY(M_ID) REFERENCES MATCHED_POST(M_ID)
+        P_ID        CHAR(10),            
+        O_ID        CHAR(10),           
+        M_ID        CHAR(10),            
+        CONSTRAINT PMFK FOREIGN KEY(P_ID) REFERENCES LOAN_POST(P_ID),
+        CONSTRAINT PMFK2 FOREIGN KEY(O_ID) REFERENCES LOAN_OFFER(O_ID),
+        CONSTRAINT PMFK3 FOREIGN KEY(M_ID) REFERENCES MATCHED_POST(M_ID)
     );
 
 -- Create the AUTHENTICATES table
 CREATE TABLE AUTHENTICATES
     (
-        AEmail      VARCHAR(20)         NOT NULL,
-        CEmail      VARCHAR(20)         NOT NULL,
-        LEmail      VARCHAR(20)         NOT NULL,
-        FOREIGN KEY(AEmail) REFERENCES ADMIN(AEmail),
-        FOREIGN KEY(CEmail) REFERENCES CLIENT(CEmail),
-        FOREIGN KEY(LEmail) REFERENCES LOAN_PROVIDER(LEmail)
+        AEmail      VARCHAR(20),
+        CEmail      VARCHAR(20),
+        LEmail      VARCHAR(20),
+        CONSTRAINT AFK FOREIGN KEY(AEmail) REFERENCES ADMIN(AEmail),
+        CONSTRAINT AFK2 FOREIGN KEY(CEmail) REFERENCES CLIENT(CEmail),
+        CONSTRAINT AFK3 FOREIGN KEY(LEmail) REFERENCES LOAN_PROVIDER(LEmail)
     );
 
 -- Create the MATCH_CHECK table
 CREATE TABLE MATCH_CHECK
     (
-        O_ID        CHAR(10)            NOT NULL,
-        P_ID        CHAR(10)            NOT NULL,
+        O_ID        CHAR(10),
+        P_ID        CHAR(10),
         MRate       INT,
-        FOREIGN KEY(O_ID) REFERENCES LOAN_OFFER(O_ID),
-        FOREIGN KEY(P_ID) REFERENCES LOAN_POST(P_ID)
+        CONSTRAINT MCFK FOREIGN KEY(O_ID) REFERENCES LOAN_OFFER(O_ID),
+        CONSTRAINT MCFK2 FOREIGN KEY(P_ID) REFERENCES LOAN_POST(P_ID)
     );
 
 -- Stored Procedure to Add Client Information
