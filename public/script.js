90d5cdf6e31c9bda55a8655786faa261281d1c43
Luftwaffles <sunnyhoang314@gmail.com>
2024-11-21 16:03:00 -0700

    Landing page prototype

---
+++ public/script.js
@@ -0,0 +1,9 @@
+// const background = document.querySelector('.background');
+
+// document.addEventListener('mousemove', (e) => {
+//     const { clientX, clientY } = e;
+//     const movementX = (clientX / window.innerWidth) * 10 - 5;
+//     const movementY = (clientY / window.innerHeight) * 10 - 5;
+
+//     background.style.transform = `translate(${movementX}px, ${movementY}px)`;
+// });
